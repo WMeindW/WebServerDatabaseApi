a705b7d6ef086893086bc58b773a852363bf49b1
Meind <105383135+WMeindW@users.noreply.github.com>
2025-01-10 13:43:59 +0100

    update

--- src/main/java/cz/meind/service/Actions.java
+++ src/main/java/cz/meind/service/Actions.java
@@ -3,12 +3,11 @@ package cz.meind.service;
 import cz.meind.database.entities.Customer;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static cz.meind.application.Application.mapper;
 
 public class Actions {
-    public static Customer register(String email, String name, String address) {
+    public static Customer signup(String email, String name, String address) {
         List<Customer> customers = mapper.fetchAll(Customer.class);
         if (!customers.stream().filter(c -> c.getName().equals(name)).toList().isEmpty()) return null;
         Customer c = new Customer();
@@ -16,5 +15,14 @@ public class Actions {
         c.setAddress(address);
         c.setName(name);
         return c;
+    }
+
+    public static Customer login(String name) {
+        List<Customer> customers = mapper.fetchAll(Customer.class);
+        return customers.stream().filter(c -> c.getName().equals(name)).findFirst().orElse(null);
+    }
+
+    public static void deleteCustomer(Customer customer) {
+        mapper.deleteById(Customer.class, customer.getId());
     }
 }
--- src/main/java/cz/meind/service/Console.java
+++ src/main/java/cz/meind/service/Console.java
@@ -1,33 +1,129 @@
 package cz.meind.service;
 
-import cz.meind.application.Application;
+import cz.meind.database.entities.Customer;
 
 import java.util.*;
 
 
 public class Console {
     private static final Scanner scanner = new Scanner(System.in);
-
+    private static Customer currentCustomer;
     private static boolean loggedIn = false;
 
     public static void run() {
         do {
             if (loggedIn) {
-
-            }else {
+                System.out.println("Logged in as: " + currentCustomer.getName());
+                System.out.print(printActions());
+            } else {
                 System.out.print(printLogin());
             }
-            Integer command = Integer.valueOf(scanner.next().strip());
+            execute(scanner.next().strip());
         } while (true);
     }
 
     private static String printLogin() {
         return """
-                Hello!
+                
                 [0] Login
-                [1] Register
+                [1] Signup
                 [Exit] Exit
                 
                 Command:""";
     }
+
+    private static String printActions() {
+        return """
+                
+                [0] View products
+                [1] Add to cart
+                [2] Pay
+                [3] Logout
+                [4] Delete account
+                [Exit] Exit
+                
+                Command:""";
+    }
+
+    private static void execute(String command) {
+        if (command.equalsIgnoreCase("exit")) System.exit(0);
+        int choice;
+        try {
+            choice = Integer.parseInt(command);
+        } catch (Exception e) {
+            System.err.println("Invalid command, number expected.");
+            return;
+        }
+        if (!loggedIn) {
+            switch (choice) {
+                case 0:
+                    login();
+                    break;
+                case 1:
+                    signup();
+                    break;
+                default:
+                    System.err.println("Invalid command, number out of range.");
+            }
+        } else {
+            switch (choice) {
+                case 0:
+                    viewProducts();
+                    break;
+                case 1:
+                    addToCart();
+                    break;
+                case 2:
+                    pay();
+                    break;
+                case 3:
+                    logout();
+                    break;
+                case 4:
+                    deleteAccount();
+                    break;
+                default:
+                    System.err.println("Invalid command, number out of range.");
+            }
+        }
+    }
+
+    private static void login() {
+        System.out.print("Name: ");
+        Customer c = Actions.login(scanner.next().strip().replace(" ", ""));
+        if (c == null) {
+            System.err.println("Invalid login.");
+            return;
+        }
+        loggedIn = true;
+        currentCustomer = c;
+    }
+
+    private static void signup() {
+
+    }
+
+    private static void viewProducts() {
+
+    }
+
+    private static void addToCart() {
+
+    }
+
+    private static void pay() {
+
+    }
+
+    private static void logout() {
+        loggedIn = false;
+        currentCustomer = null;
+    }
+
+    private static void deleteAccount() {
+        Actions.deleteCustomer(currentCustomer);
+        logout();
+        System.out.println("Account deleted.");
+    }
 }
+
